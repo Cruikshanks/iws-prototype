c0b9f59f35f72521b6e7e0ee633c98b050f553a6
liammcmurray <liammcmurray@gmail.com>
2017-03-23 15:52:22 +0000

    starting proxy journey

--- app/config.js
+++ app/config.js
@@ -5,7 +5,7 @@
 module.exports = {
 
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: "International Waste Shipments",
+  serviceName: "International Waste Shipments (IWS)",
 
   // Default port that prototype runs on
   port: '3000',
